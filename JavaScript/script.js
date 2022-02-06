8fa7ce1c454f65fd9ab6d9424a3a1f5177f84cca
Teones <teones.alex.talff99@gmail.com>
2022-02-05 23:16:55 -0300

    clique na carta e fim de jogo feitos

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,11 +7,16 @@ let imagens = [
     "revertitparrot.gif",
     "tripletsparrot.gif",
     "unicornparrot.gif"]
+
 imagens.sort(embaralhar)
+function embaralhar() {
+    return Math.random() - 0.5;
+}
+
 let cartas = []
 
 function quantasCartas() {
-    quantidadeCartas = prompt("Quantas cartas irá jogar?")
+    quantidadeCartas = prompt("Quantas cartas irá jogar? OBS: Número entre 4 e 14")
     if (quantidadeCartas % 2 !== 0 || quantidadeCartas > 14 || quantidadeCartas < 4) {
         quantasCartas()
     } else {
@@ -25,12 +30,12 @@ function disporCartas() {
         cartas.push(`
             <div class="carta" onclick="virar(this)">
                 <img src="Imagens/front.png" alt="">
-                <img src="Imagens/${imagens[i]}" class="verso" alt="">
+                <img src="Imagens/${imagens[i]}" class="esconder" alt="">
             </div>`)
         cartas.push(`
             <div class="carta" onclick="virar(this)">
                 <img src="Imagens/front.png" alt="">
-                <img src="Imagens/${imagens[i]}" class="verso" alt="">
+                <img src="Imagens/${imagens[i]}" class="esconder" alt="">
             </div>`)
         cartas.sort(embaralhar)    
     }
@@ -40,6 +45,58 @@ function disporCartas() {
     }
 }
 
-function embaralhar() {
-    return Math.random() - 0.5;
-  }
+let cartasViradas = 0
+let tipoCartas = []
+let rodadas = 0
+
+function virar(carta) {
+    rodadas += 1
+    let capa = carta.children[0]
+    capa.classList.add("esconder")
+    let gif = carta.children[1]
+    gif.classList.remove("esconder")
+
+    tipoCartas.push(gif)
+    
+    cartasViradas += 1
+    console.log(cartasViradas)
+    if (cartasViradas === 2) {
+        verificarPar()
+    }
+}
+
+let pai
+let filho
+let pontos = 0
+
+function verificarPar() {
+    if (tipoCartas[0].src === tipoCartas[1].src) {
+        cartasViradas = 0
+        tipoCartas = []
+        pontos += 1
+
+        verificarVitoria()
+    } else {
+        setTimeout(naoEhPar, 1000)
+    }
+}
+function naoEhPar() {
+    tipoCartas[0].classList.add("esconder")
+    pai = tipoCartas[0].parentNode
+    filho = pai.children[0]
+    filho.classList.remove("esconder")
+    tipoCartas[1].classList.add("esconder")
+    pai = tipoCartas[1].parentNode
+    filho = pai.children[0]
+    filho.classList.remove("esconder")
+    
+    cartasViradas = 0
+    tipoCartas = []
+}
+
+function verificarVitoria() {
+    if (pontos === (quantidadeCartas / 2)) {
+        let mensagem = `Parabéns, você ganhou em ${rodadas} rodadas`
+        alert(mensagem)
+    }
+}
